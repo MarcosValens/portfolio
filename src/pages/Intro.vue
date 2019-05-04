8f84fd609597b009abeb440f300649042b0684d0
Marcos <marcosvalens@gmail.com>
2019-05-04 23:00:29 +0200

    CV OK falta responsive

--- src/pages/Intro.vue
+++ src/pages/Intro.vue
@@ -1,8 +1,8 @@
 <template>
   <div id="q-app">
     <div class="q-pa-xl">
-      <div class="row flex justify-center" style="height: 50vh;">
-        <div class="element col-6 text-justify"></div>
+      <div class="row flex justify-center" style="height: 75vh;">
+        <div class="element col-lg-6 col-sm-12 text-justify"></div>
       </div>
       <div class="row flex justify-center">
           <q-btn
@@ -27,11 +27,11 @@
         "que sin experiencia no soy capaz de encontrar un trabajo de lo que realmente me apasiona, he decidido hacer " +
         "esta pequeña web, donde podreis ver algunas de mis cualidades de la parte “Front-End” y aunque no podáis verlo " +
         "también hay alguna cosa muy básica de la parte de “Back-End”. Recordad que si mi presentación se hace muy " +
-        "pesada siempre podéis os la podéis saltar haciendo click en el botón. Saludos!!", "Como recién graduado, me " +
+        "pesada siempre os la podéis saltar haciendo click en el botón. Saludos!!", "Como recién graduado, me " +
         "encantaría poder entrar a formar parte de una empresa en la que poder aplicar todos mis conocimientos y, al " +
         "mismo tiempo, que me permita desarrollarme profesionalmente. Gracias a mi formación especializada, considero " +
         "que puedo aportar valor y seguir desarrollándome profesionalmente en una compañía que coincida con mis valores " +
-        "y expectativas. Como persona organizada y con una gran motivación, soy capaz de adaptarme a cualquier " +
+        "y expectativas. Como persona organizada y con una gran motivación.", "Soy capaz de adaptarme a cualquier " +
         "circunstancia y dar siempre lo mejor de mí en cualquier proyecto, al mismo tiempo que me esfuerzo por trabajar " +
         "en equipo y fomentar valores como los del compañerismo. Soy una persona emprendedora, que adora los retos y no " +
         "se rinde fácilmente. Muy detallista y autodidacta cada día, nunca dejo de aprender y tengo un especial don de " +
